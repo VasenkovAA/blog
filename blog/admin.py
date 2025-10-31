08577eec2af29c9f9d859665dbc11f6887a9aa03
Andrey <andrej.500@mail.ru>
2025-10-31 15:51:18 +0300

    feat: add post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from blog.models.articles import Article, ArticleGroup
+from blog.models.post import Post
 from blog.models.projects import Project
 
 
@@ -17,3 +18,14 @@ class ArticleGroupAdmin(admin.ModelAdmin):
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ["name", "group", "created_at"]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish", "status"]
+    list_filter = ["status", "created", "publish", "author"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"
+    ordering = ["status", "publish"]
