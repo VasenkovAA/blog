c1d05df843d6aa1c126ca37b61a7721bba5769ba
Andrey <andrej.500@mail.ru>
2025-11-09 21:48:57 +0300

    feat: add postgres

--- core/settings.py
+++ core/settings.py
@@ -86,6 +86,16 @@ DATABASES = {
     }
 }
 
+if os.getenv("POSTGRES_DB"):
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
